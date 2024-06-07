43895d75fa42c34f69d74bdf54fbd777c975aa78
Gabriel lima <gabrielfelixsilva1@gmail.com>
2024-06-06 21:22:23 -0300

    calculadora dodev

--- src/script.js
+++ src/script.js
@@ -1,8 +1,39 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(numeros1, numeros2, operador) {
+  let resultado
+  let num1 = parseInt(numeros1)
+  let num2 = parseInt(numeros2)
+  
+  switch(operador){
+    case "+":
+      resultado = num1 + num2
+      break;
+    case "-":
+       resultado = num1 - num2
+      break;
+    case "/":
+       resultado = num1 / num2
+      break;
+    case "*":
+       resultado = num1 * num2
+      break;
+    case "e":
+      let contador =1 
+       resultado = num1 * num2
+      while(contador < num2 -1){
+        resultado = resultado * num1
+        contador++
+      }
+      break;
+  }
+  if(resultado == undefined || resultado> 1000000)
+    resultado = "ERRO"
 
+  return resultado
+
+};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
